337a22a4ac3d0230125caf725a9beaa0fc2209fc
Amir Shakibafar <149489707+AmirShakibafar@users.noreply.github.com>
2025-07-16 00:29:19 +0330

    fixed slight animation error on text

--- src/components/HeroContents.tsx
+++ src/components/HeroContents.tsx
@@ -1,13 +1,23 @@
-'use client';
-import { motion, Variants, useAnimationControls } from 'framer-motion';
-import Image from 'next/image';
-import AnimatedWords from './AnimatedWords';
-import AnimatedNumber from './AnimatedNumber';
-import ShinyButton from './ShinyButton';
-import React, { useEffect } from 'react';
+"use client";
+import { motion, Variants, useAnimationControls } from "framer-motion";
+import Image from "next/image";
+import AnimatedWords from "./AnimatedWords";
+import AnimatedNumber from "./AnimatedNumber";
+import ShinyButton from "./ShinyButton";
+import React, { useEffect } from "react";
 
 export default function HeroContent() {
-  const headingChild: Variants = {};
+  const headingChild: Variants = {
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", damping: 12, stiffness: 100 },
+    },
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+  };
 
   const controls = useAnimationControls();
 
@@ -18,20 +28,19 @@ export default function HeroContent() {
   }, [controls]);
 
   const imageVariants: Variants = {
-   
-    hidden: { 
+    hidden: {
       opacity: 0,
       x: "-50vw",
       scale: 0.8,
     },
-    flyIn: { 
+    flyIn: {
       opacity: 1,
       x: 0,
       scale: 1,
-      transition: { 
-        duration: 0.3, 
-        ease: "circOut"
-      }
+      transition: {
+        duration: 0.3,
+        ease: "circOut",
+      },
     },
 
     float: {
@@ -43,17 +52,20 @@ export default function HeroContent() {
         repeat: Infinity,
         repeatType: "mirror",
         ease: "easeInOut",
-      }
-    }
+      },
+    },
   };
- 
 
   return (
     <>
-      <div className="absolute top-0 right-0 h-full w-2/5 backdrop-blur-sm" style={{ maskImage: 'linear-gradient(to left, black 60%, transparent)' }} />
+      <div
+        className="absolute top-0 right-0 h-full w-2/5 backdrop-blur-sm"
+        style={{
+          maskImage: "linear-gradient(to left, black 60%, transparent)",
+        }}
+      />
       <div className="relative z-10 h-full flex items-center justify-center">
         <div className="flex items-center gap-8">
-          
           <motion.div
             variants={imageVariants}
             initial="hidden"
@@ -70,21 +82,45 @@ export default function HeroContent() {
 
           <div className="flex flex-col p-6">
             <h1 className="hero-heading">
-              <AnimatedWords text="!سلام" direction="rtl"/>
-              <motion.div className="flex justify-end" initial="hidden" animate="visible" transition={{ staggerChildren: 0.1 }}>
+              <AnimatedWords text="!سلام" direction="rtl" />
+              <motion.div
+                className="flex-row-reverse justify-end"
+                initial="hidden"
+                animate="visible"
+                transition={{ staggerChildren: 0.1 }}
+              >
                 <motion.span variants={headingChild}>(:</motion.span>
-                <motion.span variants={headingChild} className="text-red-500 mx-2">امیر</motion.span>
+                <motion.span
+                  variants={headingChild}
+                  className="text-red-500 mx-2"
+                >
+                  امیر
+                </motion.span>
                 <motion.span variants={headingChild}>هستم</motion.span>
               </motion.div>
             </h1>
-            <AnimatedWords 
-              text="کارم طراحی و توسعه محصولات دیجیتال جذاب و کاربرپسنده" 
+
+            <AnimatedWords
+              text="کارم طراحی و توسعه محصولات دیجیتال جذاب و کاربرپسنده"
               className="hero-subheading"
               direction="rtl"
             />
+
             <p className="hero-paragraph" dir="rtl">
-              <span className='text-[#E50000] font-bold'>+<AnimatedNumber value={1.5} decimals={1} /></span> سال سابقه | <span className='text-[#E50000] font-bold'><AnimatedNumber value={4} /></span> پروژه واقعی | <span className='text-[#E50000] font-bold'>+<AnimatedNumber value={36} /></span> استار گیت‌هاب
+              <span className="text-[#E50000] font-bold">
+                +<AnimatedNumber value={1.5} decimals={1} />
+              </span>{" "}
+              سال سابقه |{" "}
+              <span className="text-[#E50000] font-bold">
+                <AnimatedNumber value={4} />
+              </span>{" "}
+              پروژه واقعی |{" "}
+              <span className="text-[#E50000] font-bold">
+                +<AnimatedNumber value={36} />
+              </span>{" "}
+              استار گیت‌هاب
             </p>
+
             <div className="mt-10 flex justify-end gap-4">
               <ShinyButton text="→ دیدن نمونه کارهام" variant="secondary" />
               <ShinyButton text="→ تماس با من" />

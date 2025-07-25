6496ada6dc22da6ba43a275ca8bc3d7b3341f379
Amir Shakibafar <149489707+AmirShakibafar@users.noreply.github.com>
2025-07-26 02:43:41 +0330

    added resume section

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -1,10 +1,12 @@
 import AboutHero from "@/components/AboutHero";
 import CtaSection from "@/components/CtaSection";
+import ResumeSection from "@/components/ResumeSection";
 
 export default function AboutPage() {
   return (
     <>
       <AboutHero />
+      <ResumeSection/>
       <CtaSection />
     </>
   );
